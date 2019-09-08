b1f345006f9643260e77009b1038a8854efb1bd8
sqrtthree <imsqrtthree@gmail.com>
2019-09-09 00:06:27 +0800

    :bug: fix: Fix errors in install.sh
    Signed-off-by: sqrtthree <imsqrtthree@gmail.com>

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 set -e
 
-printf "\033[1;34m==>\033[0m Download latest scripts from GitHub"
+printf "\033[1;34m==>\033[0m Download latest scripts from GitHub\n"
 curl -fsSL https://github.com/sqrthree/motd.dynamic/archive/master.zip -o motd.dynamic.zip
 
-printf "\033[1;34m==>\033[0m Extract motd.dynamic.zip"
+printf "\033[1;34m==>\033[0m Extract motd.dynamic.zip\n"
 unzip -q motd.dynamic.zip
 
-printf "\033[1;34m==>\033[0m Extract motd.dynamic.zip"
+printf "\033[1;34m==>\033[0m Extract motd.dynamic.zip\n"
 sudo mv motd.dynamic-master /etc/motd.dynamic
 
-printf "\033[1;34m==>\033[0m Link motd.dynamic to /etc/profile.d/"
+printf "\033[1;34m==>\033[0m Link motd.dynamic to /etc/profile.d/ \n"
 sudo ln /etc/motd.dynamic/motd.dynamic /etc/profile.d/motd.dynamic.sh
 
-printf "\033[32m[✔]\033[0m Successfully to install motd.dynamic. The system info will be shown after your next login."
+printf "\033[1;34m==>\033[0m Do some cleaning up\n"
 
-cd -
+printf "\033[32m[✔]\033[0m Successfully to install motd.dynamic. The system info will be shown after your next login.\n"
